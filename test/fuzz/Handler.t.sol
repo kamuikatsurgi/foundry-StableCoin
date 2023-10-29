49fbb8134d272252a0de0f3a5abe4fa9e9b1a9db
kamuikatsurgi <shahkrishang11@gmail.com>
2023-10-29 15:34:25 +0530

    fuzz tests⭐️

---
+++ test/fuzz/Handler.t.sol
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import {Test} from "forge-std/Test.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {MockV3Aggregator} from "../mocks/MockV3Aggregator.sol";
+import {console} from "forge-std/console.sol";
+
+contract Handler is Test {
+    using EnumerableSet for EnumerableSet.AddressSet;
+
+    // Deployed contracts to interact with
+    DSCEngine public dscEngine;
+    DecentralizedStableCoin public dsc;
+    MockV3Aggregator public ethUsdPriceFeed;
+    MockV3Aggregator public btcUsdPriceFeed;
+    ERC20Mock public weth;
+    ERC20Mock public wbtc;
+
+    // Ghost Variables
+    uint96 public constant MAX_DEPOSIT_SIZE = type(uint96).max;
+
+    constructor(DSCEngine _dscEngine, DecentralizedStableCoin _dsc) {
+        dscEngine = _dscEngine;
+        dsc = _dsc;
+
+        address[] memory collateralTokens = dscEngine.getCollateralTokens();
+        weth = ERC20Mock(collateralTokens[0]);
+        wbtc = ERC20Mock(collateralTokens[1]);
+
+        ethUsdPriceFeed = MockV3Aggregator(dscEngine.getCollateralTokenPriceFeed(address(weth)));
+        btcUsdPriceFeed = MockV3Aggregator(dscEngine.getCollateralTokenPriceFeed(address(wbtc)));
+    }
+
+    function mintAndDepositCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+        amountCollateral = bound(amountCollateral, 1, MAX_DEPOSIT_SIZE);
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+
+        vm.startPrank(msg.sender);
+        collateral.mint(msg.sender, amountCollateral);
+        collateral.approve(address(dscEngine), amountCollateral);
+        dscEngine.depositCollateral(address(collateral), amountCollateral);
+        vm.stopPrank();
+    }
+
+    function redeemCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+        uint256 maxCollateral = dscEngine.getCollateralBalanceOfUser(msg.sender, address(collateral));
+
+        amountCollateral = bound(amountCollateral, 0, maxCollateral);
+        if (amountCollateral == 0) {
+            return;
+        }
+        dscEngine.redeemCollateral(address(collateral), amountCollateral);
+    }
+
+    function burnDsc(uint256 amountDsc) public {
+        amountDsc = bound(amountDsc, 0, dsc.balanceOf(msg.sender));
+        if (amountDsc == 0) {
+            return;
+        }
+        dscEngine.burnDsc(amountDsc);
+    }
+
+    function liquidate(uint256 collateralSeed, address userToBeLiquidated, uint256 debtToCover) public {
+        uint256 minHealthFactor = dscEngine.getMinHealthFactor();
+        uint256 userHealthFactor = dscEngine.getHealthFactor(userToBeLiquidated);
+        if (userHealthFactor >= minHealthFactor) {
+            return;
+        }
+        debtToCover = bound(debtToCover, 1, uint256(type(uint96).max));
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+        dscEngine.liquidate(address(collateral), userToBeLiquidated, debtToCover);
+    }
+
+    function transferDsc(uint256 amountDsc, address to) public {
+        if (to == address(0)) {
+            to = address(1);
+        }
+        amountDsc = bound(amountDsc, 0, dsc.balanceOf(msg.sender));
+        vm.prank(msg.sender);
+        dsc.transfer(to, amountDsc);
+    }
+
+    function updateCollateralPrice(uint96 newPrice, uint256 collateralSeed) public {
+        int256 intNewPrice = int256(uint256(newPrice));
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+        MockV3Aggregator priceFeed = MockV3Aggregator(dscEngine.getCollateralTokenPriceFeed(address(collateral)));
+
+        priceFeed.updateAnswer(intNewPrice);
+    }
+
+    function _getCollateralFromSeed(uint256 collateralSeed) private view returns (ERC20Mock) {
+        if (collateralSeed % 2 == 0) {
+            return weth;
+        } else {
+            return wbtc;
+        }
+    }
+}
--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -48,4 +48,46 @@ contract DSCEngineTest is Test {
         vm.stopPrank();
     }
 
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
+    function testShouldRevertIfLengthsDontMatch() public {
+        tokenAddresses.push(weth);
+        priceFeedAddresses.push(ethUsdPriceFeed);
+        priceFeedAddresses.push(btcUsdPriceFeed);
+
+        vm.expectRevert();
+        new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc)); 
+    }
+
+    function testGetTokenAmountFromUsd() public {
+        //$100 of WETH @ $2000/WETH would be 0.05 WETH
+        uint256 expectedWeth = 0.05 ether;
+        uint256 amountWeth = dsce.getTokenAmountFromUsd(weth, 100 ether);
+        assertEq(amountWeth, expectedWeth);
+    }
+
+    function testRevertsWithUnapprovedCollateral() public {
+        ERC20Mock randToken = new ERC20Mock();
+        randToken.mint(USER, 100e18);
+        vm.startPrank(USER);
+        vm.expectRevert(abi.encodeWithSelector(DSCEngine.DSCEngine__TokenNotAllowed.selector, address(randToken)));
+        dsce.depositCollateral(address(randToken), COLLATERAL_AMOUNT);
+        vm.stopPrank();
+    }
+
+    modifier depositedCollateral() {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dsce), COLLATERAL_AMOUNT);
+        dsce.depositCollateral(weth, COLLATERAL_AMOUNT);
+        vm.stopPrank();
+        _;
+    }
+    
+    function testCanDepositedCollateralAndGetAccountInfo() public depositedCollateral {
+        (uint256 totalDscMinted, uint256 collateralValueInUsd) = dsce.getAccountInformation(USER);
+        uint256 expectedDepositedAmount = dsce.getTokenAmountFromUsd(weth, collateralValueInUsd);
+        assertEq(totalDscMinted, 0);
+        assertEq(expectedDepositedAmount, COLLATERAL_AMOUNT);
+    }
 }
